641253ee4d5264d80273944825bdaaf80d76f447
SiYao Mo <1388852+msylgj@users.noreply.github.com>
2021-04-21 15:18:57 +0800

    enable openclash after sysupgrade

--- SCRIPTS/prepare_package.sh
+++ SCRIPTS/prepare_package.sh
@@ -68,7 +68,7 @@ sed -i 's/16384/65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 sed -i "s/'%D %V %C'/'Built by OPoA($(date +%Y.%m.%d))@%D %V %C'/g" package/base-files/files/etc/openwrt_release
 sed -i "/%D/a\ Built by OPoA($(date +%Y.%m.%d))" package/base-files/files/etc/banner
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-sed -i "s/openclash.config.enable=0/openclash.config.enable=1/g" package/new/luci-app-openclash/root/etc/uci-defaults/luci-openclash
+sed -i "s/openclash.config.enable=0/openclash.config.enable=1/g" feeds/luci/applications/luci-app-openclash/root/etc/uci-defaults/luci-openclash
 sed -i 's/+kmod-fast-classifier //g' package/lean/lean-translate/Makefile
 sed -i '/exit 0/i\echo "/usr/share/unblockneteasemusic/" >> /etc/sysupgrade.conf && cat /etc/sysupgrade.conf | sort | uniq > /tmp/tmp_sysupgrade_conf && cat /tmp/tmp_sysupgrade_conf > /etc/sysupgrade.conf' package/lean/lean-translate/files/zzz-default-settings
 # 生成默认配置及缓存

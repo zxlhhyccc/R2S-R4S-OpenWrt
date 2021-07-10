1b9a98274eb39634fe38a2bdfc5d7cf218c37080
SiYao Mo <1388852+msylgj@users.noreply.github.com>
2021-07-10 20:10:35 +0800

    chmod

--- SCRIPTS/target_r4s.sh
+++ SCRIPTS/target_r4s.sh
@@ -51,7 +51,6 @@ wget https://downloads.openwrt.org/releases/${latest_version}/targets/rockchip/a
 zgrep -m 1 "Depends: kernel (=.*)$" Packages.gz | sed -e 's/.*-\(.*\))/\1/' > .vermagic
 sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
 
-find ./ -name *.orig | xargs rm -f
-find ./ -name *.rej | xargs rm -f
+chmod -R 755 ./
 
-exit 0
+#exit 0

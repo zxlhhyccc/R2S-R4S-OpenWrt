73ba513632654bea9e4c696aae63dc745b3095b9
SiYao Mo <1388852+msylgj@users.noreply.github.com>
2021-10-24 21:00:29 +0800

    udpate release tag

--- SCRIPTS/get_ready.sh
+++ SCRIPTS/get_ready.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-latest_release="$(curl -s https://github.com/openwrt/openwrt/releases |grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" |sed -n '/21/p' |sed -n 1p |sed 's/.tar.gz//g')"
+latest_release="$(curl -s https://github.com/openwrt/openwrt/tags | grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" | sed -n '/21/p' | sed -n 1p | sed 's/.tar.gz//g')"
 git clone --single-branch -b ${latest_release} https://github.com/openwrt/openwrt openwrt_back
 git clone -b openwrt-21.02 --depth 1 https://github.com/immortalwrt/immortalwrt openwrt
 rm -f ./openwrt/include/version.mk
--- SCRIPTS/prepare_package.sh
+++ SCRIPTS/prepare_package.sh
@@ -57,7 +57,7 @@ svn co https://github.com/QiuSimons/OpenWrt-Add/trunk/addition-trans-zh package/
 cp -f ../SCRIPTS/zzz-default-settings package/emortal/addition-trans-zh/files/zzz-default-settings
 
 #Vermagic
-latest_version="$(curl -s https://github.com/openwrt/openwrt/releases |grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" |sed -n '/21/p' |sed -n 1p |sed 's/v//g' |sed 's/.tar.gz//g')"
+latest_version="$(curl -s https://github.com/openwrt/openwrt/tags | grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" | sed -n '/21/p' | sed -n 1p | sed 's/v//g' | sed 's/.tar.gz//g')"
 wget https://downloads.openwrt.org/releases/${latest_version}/targets/rockchip/armv8/packages/Packages.gz
 zgrep -m 1 "Depends: kernel (=.*)$" Packages.gz | sed -e 's/.*-\(.*\))/\1/' > .vermagic
 sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
